3224bf428a2928e670adff86fc5a7a4a2551f24a
Suraj Kumar <znsuraj7@gmail.com>
2022-12-10 21:18:10 +0530

    added

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -5,7 +5,9 @@ const SocketHandler = (req, res) => {
     // console.log('Socket is already running')
   } else {
     // console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
+    const io = new Server(res.socket.server,{
+      pingTimeout: 60000,
+    })
     res.socket.server.io = io
 
     io.on("connection", (socket) => {
